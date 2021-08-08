4b086b4fad1c5810b8acbde9b195146055b5d294
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-08-08 09:21:19 +0530

    # IdGenerator

--- spring-boot-rest-with-jpastreamer/src/main/java/com/ganesh/idhelper/EmployeeIdGenerator.java
+++ spring-boot-rest-with-jpastreamer/src/main/java/com/ganesh/idhelper/EmployeeIdGenerator.java
@@ -1,5 +1,28 @@
 package com.ganesh.idhelper;
 
-public class EmployeeIdGenerator {
+import java.io.Serializable;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+public class EmployeeIdGenerator implements IdentifierGenerator {
+
+	@Override
+	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+		String prefix = "EMP";
+		Integer rs = 000;
+		try {
+			int id = rs + 101;
+			String str = Integer.toString(id);
+			//String generatedId = prefix = new Integer(id).toString();
+			String generatedId = prefix + str;
+            return generatedId;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 
 }

26c31db38c2b21173603d4c7e11c442fde0b6c2e
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-08-28 11:29:25 +0530

    # initial commit

---
+++ spring-boot-with-pagingandsorting/src/main/java/com/ganesh/SpringBootWithPagingandsortingApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithPagingandsortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithPagingandsortingApplication.class, args);
+	}
+
+}
